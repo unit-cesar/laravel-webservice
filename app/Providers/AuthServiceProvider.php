a21d7b5cee0be1af87160866e1c7dadc01697705
CesarDevesa <devesa.cesar@gmail.com>
2018-09-24 18:50:56 -0300

    Generated Models and Policys: Role and Permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,3 +28,123 @@ class AuthServiceProvider extends ServiceProvider
         //
     }
 }
+/*
+ * ACL Map - Anarquico
+ *
+ * User 1 (Support)
+ * - Tudo para testes e manutenção
+ *
+ * User 2 ()
+ * -Ver tudo
+ * -Cria
+ * -Edita os seus
+ * -Deleta os seus
+ * -Publica os seus
+ * -Sugere edição os de outros
+ * -Sugere deletar os de outros
+ * -Sugere publicar os de outros
+ *
+ * */
+
+/*
+ * ACL Map - Padrão
+ *
+ * SuperUsers
+ * -Tudo
+ *
+ * PublicGuest
+ * -ver publicados
+ *
+ * Registered
+ * -ver os publicados
+ *
+ * Author
+ * -ver os publicados
+ * -cria
+ *
+ * Editor
+ * -ver os publicados
+ * -cria
+ * -edita os seus
+ * -edita os de outros
+ * -publica os seus
+ *
+ * Publisher
+ * -ver os publicados
+ * -cria
+ * -edita os seus
+ * -edita os de outros
+ * -publica os seus
+ * -publica os de outros
+ *
+ * Manager
+ * -ver os publicados
+ * -cria
+ * -edita os seus
+ * -edita os de outros
+ * -publica os seus
+ * -publica os de outros
+ * -deleta os seus
+ * -deleta os de outros
+ * -gerencia usuários(cria, deleta, edita)
+ *
+ * Administrator
+  * -ver os publicados
+ * -cria
+ * -edita os seus
+ * -edita os de outros
+ * -publica os seus
+ * -publica os de outros
+ * -deleta os seus
+ * -deleta os de outros
+ * -gerencia usuários(cria, deleta, edita)
+ * -gerencia ACL (papeis e permissões)
+ * -gerencia Configurações do Sistema
+ *
+ * */
+
+
+
+/*
+ * ACL Map - Neste projeto
+ *
+ * SuperUsers
+ * -Tudo
+ *
+ * PublicGuest
+ * -ver publicados
+ *
+ * Registered
+ * -ver os publicados
+ *
+ * Author
+ * -ver os publicados
+ * -cria
+ *
+ * Editor
+ * -ver os publicados
+ * -cria
+ * -edita os seus
+ * -edita os de outros
+ * -publica os seus
+ *
+ * Publisher
+ * -ver os publicados
+ * -cria
+ * -edita os seus
+ * -edita os de outros
+ * -publica os seus
+ * -publica os de outros
+ *
+ * Manager
+ * -ver os publicados
+ * -cria
+ * -edita os seus
+ * -edita os de outros
+ * -publica os seus
+ * -publica os de outros
+ * -deleta os seus
+ * -deleta os de outros
+ * //-gerencia usuários(cria, deleta, edita)
+ *
+ * */
